8370ec87fa018d44eabbe8d204957c03e5afe1a2
M511-52\admin <s3A432058@students.ncut.edu.tw>
2017-10-16 10:38:10 +0800

    路由群組--Route Prefixing

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::get('student/{student_no}', function ($student_no) {
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject = null) {
     return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 })->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese | english | math)']);
-*/
+
 
 //正規表達式限制參數-使用  pattern 限制
 Route::pattern('student_no','s [0-9] {10} ');
@@ -52,3 +52,15 @@ Route::get('student/{student_no}', function ($student_no) {
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject = null) {
     return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 })->where(['subject'=>'(chinese | english | math)']);
+*/
+
+//路由群組--Route Prefixing
+Route::pattern('student_no','s [0-9] {10}');
+Route::group(['prefix'=>'student'], function () {
+    Route::get('{student_no}', function ($student_no) {
+        return "學號:" . $student_no;
+    });
+        Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject = null) {
+            return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+        })->where(['subject'=>'(chinese | english | math)']);
+    });
