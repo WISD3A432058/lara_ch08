43561415f05acc91d5a9de140258475b5f8ea0c7
M511-52\admin <s3A432058@students.ncut.edu.tw>
2017-10-16 10:00:47 +0800

    選擇性路由參數

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,15 @@ Route::get('student/{student_no}', function ($student_no) {
 Route::get('student/{student_no}/score', function ($student_no) {
     return "學號:".$student_no."的所有成績";
 });
-*/
+
 
 //多個參數路由設定
 Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject) {
     return "學號:".$student_no."的".$subject."所有成績";
+});
+*/
+
+//選擇性路由參數
+Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject = null) {
+    return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 });
