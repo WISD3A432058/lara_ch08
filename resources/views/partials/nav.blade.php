f8e87e24a3049f6004a2954096d3d85951eb5295
M511-16\admin <s3A432058@student.ncut.edu.tw>
2017-10-23 19:41:47 +0800

    修改nav.blade.php內容-陳欣妤

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="{{ action('HomeController@index) }}">HelloLaravel</a>
+        <a class="navbar-brand" href="{{ action('HomeController@index') }}">HelloLaravel</a>
         <ul class="nav navbar-nav">
             <li>
-                <a href="{{ action('BoardController@getIndex) }}">排行榜</a>
+                <a href="{{ action('BoardController@getIndex') }}">排行榜</a>
             </li>
         </ul>
     </div>
