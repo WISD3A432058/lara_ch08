081d840d92c59706db59e0d3de0291ad5e39fcd8
M511-16\admin <s3A432058@student.ncut.edu.tw>
2017-10-23 19:10:49 +0800

    修改head/blade.php內容-陳欣妤

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,4 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2017/10/23
- * Time: 下午 07:03
- */
+<meta charset="UTF-8">
+Link rel="stylesheet" type="text/css" href="{{ asset('bower/bootstrap/dist/css/bootstrap.min.css') }}" />
+<script type="text/javascript" src="{{ asset('bower/jquery/dist/jquery.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('bower/bootstrap/dist/js/bootstrap.min.js') }}"></script>
