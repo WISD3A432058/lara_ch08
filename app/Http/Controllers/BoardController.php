cac157e23cd2b0c03ae61c92459ebc712a765a98
s3A432058 <s3A432058@student.ncut.edu.tw>
2017-11-28 23:46:15 +0800

    修改BoardController之getIndex()方法(可由DB取得學生的國英數成績資料且依成績高低排列後，傳遞給board視圖呈現)

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -9,8 +9,14 @@ use App\Score;
 
 class BoardController extends Controller
 {
-    public function index()
+    public function getIndex()
      {
-         return view('board');
+         //return view('board');
+
+         //可由DB取得學生的國英數成績資料($scores)，且依成績高低排列(依總分排列，再依國英數)後，傳遞給board視圖呈現
+         $scores=Score::orderByTotal()
+             ->orderBySubject()->get();
+         $data=['scores'=>$scores];
+         return view('board',$data);
      }
 }
