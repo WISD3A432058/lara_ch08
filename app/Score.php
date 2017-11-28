c09c650e4923f5a7bdb8aff9059408fee58538f4
s3A432058 <s3A432058@student.ncut.edu.tw>
2017-11-28 23:31:38 +0800

    建立兩者間的一對一關係(a Student hasOne Score, a Score belongTo a Student)及設定 model 對應到的資料表

--- app/Score.php
+++ app/Score.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Score extends Model
 {
-    //
+    //設定 model 對應到的資料表
+    protected $table = 'scores';
+
+    //a Score belongTo a Student
+    public function student(){
+        return $this->belongsTo(StudentEloquent::class);
+    }
 }
--- app/Student.php
+++ app/Student.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    //設定 model 對應到的資料表
+    protected $table = 'students';
+
+    //a Student hasOne Score
+    public function score(){
+        return $this->hasOne(ScoreEloquent::class);
+    }
 }
