1fe1b601ba76a7dd06b9aae425f63600a3426d34
s3A432058 <s3A432058@student.ncut.edu.tw>
2017-11-28 10:37:25 +0800

    修改studentcontroller兩個方法

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -9,17 +9,13 @@ use View;
 
 class StudentController extends Controller
 {
-    public function getStudentData($student_no){
-        return View::make('score',[
-            'student_no'=>$student_no,
-            'subject'=>null
-        ]);
-    }
+    public function getStudentData($student_no)
+     {
+         return View::make('score',['student_no'=>$student_no,'subject'=>null]);
+      }
 
-    public function getStudentScore ($student_no=null ,$subject = null){
-        return View::make('score',[
-            'student_no'=>$student_no,
-            'subject'=>$subject
-        ]);
-    }
+    public function getStudentScore($student_no,$subject=null)
+    {
+        return view::make('score',['student_no'=>$student_no,'subject'=>$subject]);
+      }
 }
